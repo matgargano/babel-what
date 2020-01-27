325d3ee079d1ad82ee979e16649a7bde2bfc487e
Arik Waisman <arik.waisman@ibm.com>
2020-01-27 16:29:55 -0500

    resolve those Pees

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import AsyncTest from "./fetcher/AsyncTest";
 
 const r = new AsyncTest();
-const d = r.get('baz');
-console.log(d);
+const d = r.get('baz'); // cant await this because its not wrapped in an async function
+d.then(r => {
+    console.log('here',r)
+})
